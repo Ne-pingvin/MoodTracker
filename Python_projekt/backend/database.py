cf318fbec9efd98a93de18e8507495015afa5099
Ne-pingvin <161724105+Ne-pingvin@users.noreply.github.com>
2026-06-17 11:45:28 +0200

    Add files via upload

---
+++ Python_projekt/backend/database.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SERVER = "localhost"
+DATABASE = "MoodTracker"
+
+connection_string = (
+    f"mssql+pyodbc://@{SERVER}/{DATABASE}"
+    "?driver=ODBC+Driver+17+for+SQL+Server"
+    "&trusted_connection=yes"
+)
+
+engine = create_engine(
+    connection_string,
+    echo=True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
